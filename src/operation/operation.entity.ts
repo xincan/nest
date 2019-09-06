7562480d007070f3f21ee93a93f4c88d6676b4fa
jiangxincan <jiangxincan@hatech.com.cn>
2019-09-06 20:23:31 +0800

    操作功能添加code编码用于前端作为点击事件名称

--- src/operation/operation.entity.ts
+++ src/operation/operation.entity.ts
@@ -9,13 +9,18 @@ export class Operation extends BaseEntity {
     @PrimaryGeneratedColumn({name: 'id', comment: '操作ID'})
     id: number;
 
-    @Column('varchar', { name: 'operation_name', comment: '按钮名称', nullable: true })
+    @Column('varchar', { name: 'operation_name', comment: '按钮名称', nullable: true, length: 50 })
     operationName: string;
 
-    @Column('varchar', { name: 'icon', comment: '按钮图标', nullable: true })
+    @Column('varchar', { name: 'code', comment: '按钮编码（用于前端触发按钮调用函数名称）', nullable: false, length: 50 })
+    code: string;
+
+    @Column('varchar', { name: 'icon', comment: '按钮图标', nullable: true ,length: 50})
     icon: string;
 
-    @Column('varchar', { name: 'type', comment: '按钮类型(view:页面按钮（包括检索条件按钮，表格头部按钮），table:表格右侧操作列按钮)', nullable: true })
+    @Column('varchar',
+        { name: 'type', comment: '按钮类型(view:页面按钮（包括检索条件按钮，表格头部按钮），table:表格右侧操作列按钮)', nullable: true ,length: 50 }
+    )
     type: number;
 
     @Column('smallint', { name: 'sequence', comment: '按钮顺序', nullable: true })
